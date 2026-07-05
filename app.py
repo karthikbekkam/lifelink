4e857b726d1c2be7304fdf434e40e02262c4ffbb
Bekkam Karthik <bekkamkarthik366@gmail.com>
2026-07-05 14:49:14 +0530

    Auto create admin on startup

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from extensions import bcrypt
 from extensions import login_manager
 from extensions import mail
 
+from models.user import User
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
@@ -41,7 +43,61 @@ app.register_blueprint(donor_bp)
 app.register_blueprint(needer_bp)
 
 with app.app_context():
+
     db.create_all()
+
+    admin = User.query.filter_by(
+        email="admin@gmail.com"
+    ).first()
+
+    if not admin:
+
+        hashed_password = bcrypt.generate_password_hash(
+            "admin123"
+        ).decode("utf-8")
+
+        admin = User(
+
+            full_name="Administrator",
+
+            email="admin@gmail.com",
+
+            phone="9999999999",
+
+            password=hashed_password,
+
+            role="Admin",
+
+            blood_group="O+",
+
+            age=30,
+
+            gender="Male",
+
+            weight=70,
+
+            city="Vijayawada",
+
+            state="Andhra Pradesh",
+
+            address="LifeLink Admin Office",
+
+            pincode="520007",
+
+            available=True,
+
+            active=True
+
+        )
+
+        db.session.add(admin)
+        db.session.commit()
+
+        print("===================================")
+        print("Admin Created Successfully!")
+        print("===================================")
+        print("Email    : admin@gmail.com")
+        print("Password : admin123")
 
 if __name__ == "__main__":
     app.run(debug=True)
